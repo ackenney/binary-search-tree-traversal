3ce51b7b52967c2cf2f91476b4405f38178de489
ackenney <ackenney@gmail.com>
2023-04-19 18:43:59 -0400

    make BinarySearchTree class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,34 @@
+// This program traverses a Binary Search Tree in-order, pre-order,and post-order
+
 #include<iostream>
+#include<fstream>
+
+using std::ofstream;
+using std::ifstream;
+using std::cout;
+
+
+
+class BinarySearchTree
+{
+
+private:
+	ofstream outFile;
+	struct node
+	{
+		int data;
+		node* leftSubTree;
+		node* rightSubTree;
+
+	};// End of struct
+
+	node* root;// root pointer
+
+public:
+	BinarySearchTree();
+};
+
+
 
 int main()
 {
